7ee1e3178b85eb1a540b2b9406e04f21eaba05f1
Tim Henderson <tim.tadh@gmail.com>
2010-02-21 23:09:22 -0500

    can cross import
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ btree/test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+// import . "blockfile/blockfile"
+// import . "blockfile/keyblock"
+// import . "blockfile/buffers"
+// import . "blockfile/byteslice"
+
+func main() {
+    fmt.Println("test2 yoyo")
+}
