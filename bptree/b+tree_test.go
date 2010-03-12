9172898d9ee7a9615369f42eaf54edb5f0a17aef
Tim Henderson <tim.tadh@gmail.com>
2010-03-12 16:23:53 -0500

    added tests for allocate, and getblock
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- bptree/b+tree_test.go
+++ bptree/b+tree_test.go
@@ -2,8 +2,10 @@ package bptree
 
 import "testing"
 import "os"
-import "fmt"
+// import "fmt"
 import "treeinfo"
+import . "block/byteslice"
+import "block/dirty"
 
 var rec [][]byte = &([3][]byte{&[1]byte{1}, &[1]byte{1}, &[2]byte{1, 2}})
 var BLOCKSIZE uint32 = treeinfo.BLOCKSIZE
@@ -22,10 +24,46 @@ func TestCreate(t *testing.T) {
     t.Log("------- TestCreate -------")
     self := makebptree(BLOCKSIZE, t)
     defer cleanbptree(self)
-    fmt.Println(self)
 }
 
 // TODO write tests for allocate, getblock, and finding the next block in an internal node
-// func TestAllocate(t *testing.T) {
-//
-// }
+func TestAllocate(t *testing.T) {
+    t.Log("------- TestAllocate -------")
+    self := makebptree(BLOCKSIZE, t)
+    defer cleanbptree(self)
+    if self.allocate(self.internal) == nil {
+        t.Error("Allocate returned nil for internal")
+    }
+    if self.allocate(self.external) == nil {
+        t.Error("Allocate returned nil for external")
+    }
+}
+
+func TestGetBlock(t *testing.T) {
+    t.Log("------- TestGetBlock -------")
+    self := makebptree(BLOCKSIZE, t)
+    defer cleanbptree(self)
+    dirty := dirty.New(self.info.Height() * 4)
+    b1 := self.allocate(self.internal)
+    b2 := self.allocate(self.external)
+    dirty.Insert(b1)
+    dirty.Insert(b2)
+    dirty.Sync()
+
+    b1_ := self.getblock(b1.Position())
+    b2_ := self.getblock(b2.Position())
+    if b1_ == nil || b2_ == nil {
+        t.Error("getblock return nil")
+    }
+    b1s, _ := b1.Serialize()
+    b2s, _ := b2.Serialize()
+    b1_s, _ := b1_.Serialize()
+    b2_s, _ := b2_.Serialize()
+
+    if !ByteSlice(b1s).Eq(ByteSlice(b1_s)) {
+        t.Error("block read from file is not the same as the one written out for b1")
+    }
+    if !ByteSlice(b2s).Eq(ByteSlice(b2_s)) {
+        t.Error("block read from file is not the same as the one written out for b2")
+    }
+}
