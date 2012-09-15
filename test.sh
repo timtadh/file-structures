7b1c47b0f1b47f92ba92c1731ba0a26ba326dd02
Tim Henderson <tim.tadh@gmail.com>
2012-09-15 15:30:26 -0400

    tester script now builds the images
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ rm -rf dot png
 rm test.btree test.bptree
 find . -name "*.dot" | xargs rm
 
-for dir in `find . -type d -regextype egrep -regex "^./(b|t).*"`
+for dir in `find . -type d -regextype egrep -regex "^./[a-zA-Z].*"`
 do
   echo ${dir:2}
   go test file-structures/${dir:2}
@@ -15,6 +15,7 @@ do
   mv $file dot/
 done
 
+mkdir png
 for file in dot/*.dot
 do
   echo $file | cut -d "/" -f 2 - | xargs -I"%s" dot -Tpng $file -o png/%s.png
