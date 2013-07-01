555b97630c4e52e15d115392455330122c148ef2
Tim Henderson <tim.tadh@gmail.com>
2013-07-01 12:32:26 -0400

    started test for linhash
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- linhash/linhash.go
+++ linhash/linhash.go
@@ -2,49 +2,71 @@ package linhash
 
 import (
     "fmt"
+    "hash/fnv"
 )
 
 import (
     bs "file-structures/block/byteslice"
     file "file-structures/block/file2"
+    bucket "file-structures/linhash/bucket"
 )
 
-
-type ctrlblk struct {
+func hash(data []byte) uint64 {
+    fnv := fnv.New64a()
+    fnv.Write(data)
+    return fnv.Sum64()
 }
 
-const CONTROLSIZE = 0
+type ctrlblk struct {
+    buckets uint32 "number of buckets"
+    records uint64 "number of records"
+    table int64 "key of bucket translation table"
+    i uint8 "number of bits of H(.)"
+}
+
+const CONTROLSIZE = 21
 func (self *ctrlblk) Bytes() []byte {
     bytes := make([]byte, CONTROLSIZE)
+    copy(bytes[0:4], bs.ByteSlice32(self.buckets))
+    copy(bytes[4:12], bs.ByteSlice64(self.records))
+    copy(bytes[12:20], bs.ByteSlice64(uint64(self.table)))
+    bytes[20] = self.i
     return bytes
 }
 
-func load_ctrlblk(bytes []byte) (cb *ctrlblk, err error) {
+func load_ctrlblk(bytes bs.ByteSlice) (cb *ctrlblk, err error) {
     if len(bytes) < CONTROLSIZE {
         return nil, fmt.Errorf("len(bytes) < %d", CONTROLSIZE)
     }
     cb = &ctrlblk{
+        buckets: bytes[0:4].Int32(),
+        records: bytes[4:12].Int64(),
+        table: int64(bytes[12:20].Int64()),
+        i: bytes[20],
     }
     return cb, nil
 }
 
 type LinearHash struct {
     file file.BlockDevice
+    kv   bucket.KVStore
     ctrl ctrlblk
 }
 
-func NewLinearHash(file file.BlockDevice) (self *LinearHash, err error) {
+func NewLinearHash(file file.BlockDevice, kv bucket.KVStore) (self *LinearHash, err error) {
     self = &LinearHash{
         file: file,
+        kv: kv,
         ctrl: ctrlblk{
         },
     }
     return self, self.write_ctrlblk()
 }
 
-func OpenLinearHash(file file.BlockDevice) (self *LinearHash, err error) {
+func OpenLinearHash(file file.BlockDevice, kv bucket.KVStore) (self *LinearHash, err error) {
     self = &LinearHash{
         file: file,
+        kv: kv,
     }
     if err := self.read_ctrlblk(); err != nil {
         return nil, err
@@ -73,12 +95,28 @@ func (self *LinearHash) read_ctrlblk() error {
     return nil
 }
 
+func (self *LinearHash) bucket(hash uint64) uint64 {
+    i := uint64(self.ctrl.i)
+    n := uint64(self.ctrl.buckets)
+    m := hash & ((1<<i)-1) // last i bits of hash as bucket number m
+    if m < n {
+        return m
+    } else {
+        m = m ^ (1<<(i-1)) // unset the top bit
+        if m < n {
+            panic(fmt.Errorf("Expected m < self.ctrl.buckets, got %d < %d", m, self.ctrl.buckets))
+        }
+        return m
+    }
+}
+
 func (self *LinearHash) Has(key bs.ByteSlice) (has bool, error error) {
     return false, fmt.Errorf("Has Unimplemented")
 }
 
 func (self *LinearHash) Put(key bs.ByteSlice, value bs.ByteSlice) (err error) {
-    return fmt.Errorf("Put Unimplemented")
+    hash := hash(key)
+    return fmt.Errorf("Put Unimplemented %v %v", key, hash)
 }
 
 func (self *LinearHash) Get(key bs.ByteSlice) (value bs.ByteSlice, err error) {
--- linhash/linhash_test.go
+++ linhash/linhash_test.go
@@ -11,9 +11,11 @@ import (
     bs "file-structures/block/byteslice"
     file "file-structures/block/file2"
     buf "file-structures/block/buffers"
+    bucket "file-structures/linhash/bucket"
 )
 
-const PATH = "/tmp/__y"
+const PATH = "/tmp/__lin_hash"
+const VPATH = "/tmp/__varchar_store_lin_hash"
 
 func init() {
     if urandom, err := os.Open("/dev/urandom"); err != nil {
@@ -26,9 +28,23 @@ func init() {
     }
 }
 
-func testfile(t *testing.T) file.BlockDevice {
+func randslice(length int) bs.ByteSlice {
+    if urandom, err := os.Open("/dev/urandom"); err != nil {
+        panic(err)
+    } else {
+        slice := make([]byte, length)
+        if _, err := urandom.Read(slice); err != nil {
+            panic(err)
+        }
+        urandom.Close()
+        return slice
+    }
+    panic("unreachable")
+}
+
+func testfile(t *testing.T, path string) file.BlockDevice {
     const CACHESIZE = 1000
-    ibf := file.NewBlockFile(PATH, &buf.NoBuffer{})
+    ibf := file.NewBlockFile(path, &buf.NoBuffer{})
     if err := ibf.Open(); err != nil {
         t.Fatal(err)
     }
@@ -40,9 +56,69 @@ func testfile(t *testing.T) file.BlockDevice {
 }
 
 func TestNewLinearHash(t *testing.T) {
-    hash, err := NewLinearHash(testfile(t))
+    g := testfile(t, VPATH)
+    defer g.Close()
+    store, err := bucket.NewVarcharStore(g)
+    if err != nil { panic(err) }
+    hash, err := NewLinearHash(testfile(t, PATH), store)
     defer hash.Close()
     if err != nil {
         t.Fatal(err)
     }
 }
+
+func TestPutHasGetRemoveLinearHash(t *testing.T) {
+    const RECORDS = 300
+    g := testfile(t, VPATH)
+    defer g.Close()
+    store, err := bucket.NewVarcharStore(g)
+    if err != nil { panic(err) }
+    hash, err := NewLinearHash(testfile(t, PATH), store)
+    defer hash.Close()
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    type record struct {
+        key bs.ByteSlice
+        value bs.ByteSlice
+    }
+
+    keyset := make(map[uint64]bool)
+    var records []*record
+    var values2 []bs.ByteSlice
+    for i := 0; i < RECORDS; i++ {
+        key := randslice(8)
+        for {
+            if _, has := keyset[key.Int64()]; !has {
+                break
+            }
+            key = randslice(8)
+        }
+        keyset[key.Int64()] = true
+        records = append(records, &record{key, randslice(255)})
+        values2 = append(values2, randslice(255))
+    }
+
+    for _, record := range records {
+        err := hash.Put(record.key, record.value)
+        if err != nil { t.Fatal(err) }
+    }
+
+    if hash.ctrl.records != RECORDS {
+        t.Fatalf("Expected record count == %d got %d", RECORDS,
+          hash.ctrl.records)
+    }
+
+    for _, record := range records {
+        has, err := hash.Has(record.key)
+        if err != nil { t.Fatal(err) }
+        if !has { t.Fatal("Expected key") }
+        value, err := hash.Get(record.key)
+        if err != nil { t.Fatal(err) }
+        if !value.Eq(record.value) {
+            t.Fatal("Error getting record, value was not as expected")
+        }
+    }
+}
+
